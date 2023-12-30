1dc00ca1d2cf51444d8f0dcdb7fa0d05eabe4de0
YuliyaVoronovich <Y-1306@mail.ru>
2023-12-30 14:18:13 +0300

    feat: add solution getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -433,8 +433,15 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.reduce((arr, item, index) => {
+    const newResult = arr;
+
+    if (item % 2 !== 0) {
+      newResult.push(index);
+    }
+    return newResult;
+  }, []);
 }
 
 /**
