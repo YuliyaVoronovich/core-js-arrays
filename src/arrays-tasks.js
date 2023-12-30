269551275983fb172394c0629cbeeb51567b757c
YuliyaVoronovich <Y-1306@mail.ru>
2023-12-30 12:25:28 +0300

    feat: add solution generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -345,8 +345,10 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const arr = new Array(len);
+  arr.fill(1);
+  return arr.map((value, index) => 2 * index + 1);
 }
 
 /**
